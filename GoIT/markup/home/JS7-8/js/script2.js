5689bd8e6538121f6a483ceaa0eccc9ef0933694
Vsevolod <sevik.1989@gmail.com>
2016-08-24 17:17:49 +0300

    Update script2.js

--- GoIT/markup/home/JS7-8/js/script2.js
+++ GoIT/markup/home/JS7-8/js/script2.js
@@ -1,11 +1,33 @@
-$(document).ready(function(){
-  $('span').hide();
-    $(".labels #text").hover(function() {
-        $(this).next("span").animate({opacity: "show", top: "-55"}, "slow");
-    }, function() {
-        $(this).next('span').animate({opacity: "hide", top: "-85"}, "fast");
-    });
-    $('#button').click(function(){
-      $('span').slideToggle();
-    });
+$(function() {
+
+	$('.form-group input').hover(showHelp, hideHelp);
+
+function showHelp() {
+
+	var $text = $(this).attr('title');
+	var $elem = $(this).parent().parent();
+
+	if(!$elem.find('.help').length) {
+		$elem.append('<div class="col-sm-4 help"></div>');
+		$elem.find('.help').text($text).animate({
+			opacity: '1',
+			},500);
+	}
+}
+
+function hideHelp() {
+
+	var $elem = $(this).parent().parent();
+
+	$elem.find('.help').remove();
+}
+
+$('.btn-secondary').on('click', function(e) {
+	e.preventDefault();
+	if (!$('.help').length) {
+	$('.form-group input').each(showHelp);
+	}
+
+});
+
 });
