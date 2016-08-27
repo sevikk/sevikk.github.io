090c6faadf07a7c4635208b107f86a38d9b54d33
Vsevolod <sevik.1989@gmail.com>
2016-08-27 14:46:44 +0300

    Update script.js

--- GoIT/markup/home/JS11-12/js/script.js
+++ GoIT/markup/home/JS11-12/js/script.js
@@ -1,29 +1,21 @@
-console.log("test")
+$(function(){
 
-$(document).ready(function() {
-    var leftUIEl = $('.carousel-arrow-left');
-    var rightUIEl = $('.carousel-arrow-right');
-    var elementsList = $('.carousel-list');
-
-    var pixelsOffset = 425;
-    var currentLeftValue = 0;
-    var elementsCount = elementsList.find('li').length;
-    var minimumOffset = - ((elementsCount - 1) * pixelsOffset);
-    var maximumOffset = 0;
-
-    leftUIEl.click(function() {
-        if (currentLeftValue != maximumOffset) {
-            currentLeftValue += 425;
-            elementsList.animate({ left : currentLeftValue + "px"}, 500);
-        }
-    });
-
-    rightUIEl.click(function() {
-        if (currentLeftValue != minimumOffset) {
-            currentLeftValue -= 425;
-            elementsList.animate({ left : currentLeftValue + "px"}, 500);
-        }
-    });
+  $(".carousel-arrow-right").click(function(){ // при клике на правую кнопку запускаем следующую функцию:
+    $(".carousel-list").animate({left: "-125px"}, 200); // производим анимацию: блок с набором картинок уедет влево на 222 пикселя (это ширина одного прокручиваемого элемента) за 200 милисекунд.
+    setTimeout(function () { // устанавливаем задержку времени перед выполнением следующих функций. Задержка нужна, т.к. эти ффункции должны запуститься только после завершения анимации.
+      $(".carousel-list .carousel-element").eq(0).clone().appendTo(".carousel-list"); // выбираем первый элемент, создаём его копию и помещаем в конец карусели
+      $(".carousel-list .carousel-element").eq(0).remove(); // удаляем первый элемент карусели   
+      $(".carousel-list").css({"left":"0px"}); // возвращаем исходное смещение набора набора элементов карусели
+    }, 300);
+  });
+  
+  $(".carousel-arrow-left").click(function(){ // при клике на левую кнопку выполняем следующую функцию:    
+    $(".carousel-list .carousel-element").eq(-1).clone().prependTo(".carousel-list"); // выбираем последний элемент набора, создаём его копию и помещаем в начало набора  
+    $(".carousel-list").css({"left":"-125px"}); // устанавливаем смещение набора -222px    
+    $(".carousel-list").animate({left: "0px"}, 200); // за 200 милисекунд набор элементов плавно переместится в исходную нулевую точку
+    $(".carousel-list .carousel-element").eq(-1).remove(); // выбираем последний элемент карусели и удаляем его
+  });
+  
 });
 
 $(function(){
