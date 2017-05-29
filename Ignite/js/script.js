05082f8feffd3c520182a6511b5ee3a76e95a624
Vsevolod <sevik.1989@gmail.com>
2017-05-30 02:53:39 +0300

    Update script.js

--- Ignite/js/script.js
+++ Ignite/js/script.js
@@ -1,15 +1,15 @@
-$(function() {
-    $('.header_slider').owlCarousel({
+jQuery(function() {
+    jQuery('.header_slider').owlCarousel({
         loop:true,
         items: 1,
         nav:false,
         dots:true,
     })
-    $('.stories_slider').owlCarousel({
+    jQuery('.stories_slider').owlCarousel({
         items: 1,
         nav:true,
         dots:true,
         nav: true,
-        navText: ["<img src='img/back.svg'>","<img src='img/next.svg'>"]
+        navText: ["<img src='../img/back.svg'>","<img src='../img/next.svg'>"]
     })
 });
