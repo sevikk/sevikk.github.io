e821868a214b268d2988e33258fb8cccd41fc8f0
Vsevolod <sevik.1989@gmail.com>
2017-05-31 02:14:29 +0300

    Update script.js

--- Ignite/js/script.js
+++ Ignite/js/script.js
@@ -1,15 +1,29 @@
-jQuery(function() {
-    jQuery('.header_slider').owlCarousel({
+$(function($) {
+    $('.header_slider').owlCarousel({
         loop:true,
         items: 1,
         nav:false,
         dots:true,
     })
-    jQuery('.stories_slider').owlCarousel({
+    $('.stories_slider').owlCarousel({
         items: 1,
         nav:true,
         dots:true,
         nav: true,
-        navText: ["<img src='../img/back.svg'>","<img src='../img/next.svg'>"]
+        navText: ["<img src='img/back.svg'>","<img src='img/next.svg'>"]
+    });
+
+    $('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+
+    $('.hide_button').click(function(){
+        $('.menu_hidden').toggleClass('show')
     })
 });
