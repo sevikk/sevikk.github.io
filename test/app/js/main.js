c17f12b37876fc2354ed767929fa5f6f3ef8d9f6
Seva <vkuznetsov@vareger.com>
2017-08-19 15:16:32 +0300

    add index-t file + css

--- test/app/js/main.js
+++ test/app/js/main.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function ($) {
 
-    $(document).on('click', '#like', function () {
+    $(document).on('click', '.like', function () {
         if ($(this).hasClass('fa-heart-o')) {
             $(this).removeClass('fa-heart-o').addClass('fa-heart')
         } else {
@@ -8,5 +8,33 @@ jQuery(document).ready(function ($) {
         }
     })
 
+    var columns = $('.about_box');
 
+    function setEqualHeight(columns) {
+        var tallestcolumn = 0;
+        // columns.height();
+        columns.each(function () {
+            currentHeight = $(this).height();
+            if (currentHeight > tallestcolumn) {
+                tallestcolumn = currentHeight;
+            }
+        });
+        columns.height(tallestcolumn);
+    }
+
+    setEqualHeight(columns);
+    setTimeout(setEqualHeight, columns, 100);
+
+
+    // $(document).ready(function () {
+    //     setEqualHeight(columns);
+    //     orient();
+    //     setTimeout(setEqualHeight, columns, 100);
+    // });
+
+
+});
+jQuery(window).resize(function () {
+    setEqualHeight(columns);
+    setTimeout(setEqualHeight, columns, 100);
 });
