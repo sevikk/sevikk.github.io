6ee102e27d1ecd8435cf8721878c43def5a6a116
sevikk <sevik.1989@gmail.com>
2019-08-08 12:42:33 +0300

    delete old projects

--- Tech/MR/build/js/script.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	$('.main_choose').click(function() {
-	        $('.active').fadeIn("fast");
-	    });
-
-	$('.img_right, .img_left').click(function() {
-	        $('.active').fadeOut("fast");
-	    });
-});
